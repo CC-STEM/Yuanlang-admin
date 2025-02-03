2006cae3a4be3e1808c09211679d0bbb9378252c
luke <>
2025-02-03 22:19:46 +0800

    fix: fix

--- src/api/modules/goodsOrder.ts
+++ src/api/modules/goodsOrder.ts
@@ -6,4 +6,6 @@ export default {
   deleteGoodsOrder: (data: { orderNo: string }) => api.post('goodsOrder/deleteGoodsOrder', data),
   // updateGoodsOrder: (data: API.UpdateGoodsOrderBody) => api.post('goodsOrder/update', data),
   createGoodsOrder: (data: API.CreateGoodsOrderBody) => api.post('goodsOrder/createGoodsOrder', data),
+  closeGoodsOrder: (data: { orderNo: string }) => api.post('goodsOrder/closeGoodsOrderByAdmin', data),
+  orderShipping: (data: { orderNo: string, expressTrackingNo: string }) => api.post('goodsOrder/orderShipping', data),
 }
